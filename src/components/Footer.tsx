36604191d5b3660c72127187c914388493771d98
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2025-03-16 16:53:11 -0400

    migrate css to tailwind terminated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
       <div className="flex flex-row justify-center text-center -mt-14 lg:-mt-16">
         <footer className="grid text-xs sm:text-sm lg:text-base">
           <p>© 2025 Dylan. 🌐 Código abierto bajo MIT.</p>
-          <p>Diseñado y desarrollado con ❤️</p>
+          <p>Diseñado y desarrollado con ❤️</p> 
         </footer>
       </div>
     </>
