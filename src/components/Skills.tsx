3bac6d4ee1f4da463fc34441a47d35fdc921645e
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2025-03-16 12:33:38 -0400

    migrate css to tailwind profile, projects and skill terminated

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,27 +1,35 @@
-import skills from "../assets/styles/Skill.module.css";
 import { skillsHook } from "../hooks/skillsHook";
 
 const Array = ({ nombre }: { nombre: string }) => {
   return (
     <>
-      <div className={skills.circle}></div>
-      <span className={skills.arra}>{nombre}</span>
+      
+      <span className="mt-1 ml-0.5 pr-1 text-md md:text-lg">{nombre}</span>
     </>
   );
 };
 
 export const Skills = () => {
   const [array] = skillsHook();
-  console.log(array);
+  array.map((item) => (
+    console.log(item.colors)));
   return (
     <>
-      <div className={skills["responsive-web"]}>
-        <h2 className={skills.title}>Habilidades</h2>
-        <p className={skills.des}>Tecnologías y herramientas que domino.</p>
-        <div className={skills.gridContainer}>
+      <div className="p-6">
+        <h2 className="text-4xl text-white md:text-5xl lg:text-center">
+          Habilidades
+          </h2>
+        <p className="text-xl text-stone-400 md:text-2xl lg:text-center lg:mb-10">Tecnologías y herramientas que domino.</p>
+        <div className="grid grid-cols-3 space-x-3 -mr-2 -pl-3 sm:grid-cols-4
+         sm:w-154 md:grid-cols-2 md:w-180 lg:m-auto lg:grid-cols-4 
+         ">
           {array.map((item) => (
-            <div className={skills.container} key={item.id}>
-            <Array nombre={item.nombre}></Array>
+            <div className={`mt-5 border border-stone-800 flex rounded-lg p-1
+            sm:mt-4 hover:scale-105 transition-transform duration-400 cursor-pointer ${item.colors}
+             ${item.textColor}`}
+            key={item.id}>
+            <img src={item.image} alt="" 
+            className="w-5 mt-[0.1rem]"/> <Array nombre={item.nombre}></Array> 
             </div>
           ))}
         </div>
--- src/hooks/skillsHook.ts
+++ src/hooks/skillsHook.ts
@@ -1,18 +1,28 @@
 import { useState } from "react"
+import python from "../assets/images/svg/python.svg";
+import ts from "../assets/images/svg/ts.svg";
+import js from "../assets/images/svg/js.svg";
+import c from "../assets/images/svg/c.svg";
+import cpp from "../assets/images/svg/cpp.svg";
+import git from "../assets/images/svg/git.svg";
+import bash from "../assets/images/svg/bash.svg";
+import posgresql from "../assets/images/svg/postgresql.svg";
+import react from "../assets/images/svg/react.svg";
+import node from "../assets/images/svg/nodo-js.svg";
+
 
 export const skillsHook = () => {
     const listadoSkill = [
-      {id: 0, nombre: "Python"},
-      {id: 1, nombre: "TypeScript"},
-      {id: 2, nombre: "JavaScript"},
-      {id: 3, nombre: "C"},
-      {id: 4, nombre: "C++"},
-      {id: 5, nombre: "Git"},
-      {id: 6, nombre: "Bash"},
-      {id: 7, nombre: "PostgreSQL"},
-      {id: 8, nombre: "React"},
-      {id: 9, nombre: "Node.JS"},
-
+      {id: 0, nombre: "Python", image:python, colors: "hover:bg-yellow-300", textColor: "hover:text-black"},
+      {id: 1, nombre: "TypeScript", image:ts, colors: "hover:bg-cyan-950"},
+      {id: 2, nombre: "JavaScript", image:js, colors: "hover:bg-yellow-300", textColor: "hover:text-black"},
+      {id: 3, nombre: "C", image:c, colors: "hover:bg-blue-950"},
+      {id: 4, nombre: "C++", image:cpp, colors: "hover:bg-blue-900"},
+      {id: 5, nombre: "Git", image:git, colors: "hover:bg-orange-300", textColor: "hover:text-black"},
+      {id: 6, nombre: "Bash", image:bash, colors: "hover:bg-slate-500"},
+      {id: 7, nombre: "PostgreSQL", image:posgresql, colors: "hover:bg-slate-900"},
+      {id: 8, nombre: "React", image:react, colors: "hover:bg-sky-800"},
+      {id: 9, nombre: "NodeJS",image:node, colors: "hover:bg-emerald-900"},
     ]
     const [array, setArray] = useState(listadoSkill);
     setArray;
