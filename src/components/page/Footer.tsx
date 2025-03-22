b79108e8e78db1394d9bc77207b8b24d627cb4e6
Dylan <150361945+DumbNoxx@users.noreply.github.com>
2025-03-22 01:46:23 -0400

    fix conflict

--- src/components/page/Footer.tsx
+++ src/components/page/Footer.tsx
@@ -1,13 +1,8 @@
 export const Footer = () => {
   return (
     <>
-<<<<<<< HEAD:src/components/Footer.tsx
-      <div className="flex flex-row justify-center text-center -mt-14 lg:-mt-16">
-        <footer className="grid mb-4 text-xs mr-7 sm:mx-auto sm:text-sm lg:text-base lg:mb-6">
-=======
       <div className="flex flex-row justify-center text-center mb-4 -mt-14 lg:-mt-16">
         <footer className="grid text-xs sm:text-sm lg:text-base">
->>>>>>> dev:src/components/page/Footer.tsx
           <p>© 2025 Dylan. 🌐 Código abierto bajo MIT.</p>
           <p>Diseñado y desarrollado con ❤️</p> 
         </footer>
