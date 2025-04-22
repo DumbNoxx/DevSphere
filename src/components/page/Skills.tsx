b1a3b852c28b5faf14ec0c6db28c33ebfc1702a7
Nxus <150361945+DumbNoxx@users.noreply.github.com>
2025-04-22 13:38:08 -0400

    FIX: tabs skills

--- src/components/page/Skills.tsx
+++ src/components/page/Skills.tsx
@@ -18,11 +18,7 @@ export const Skills = () => {
         <h3 className="text-xl text-stone-400 md:text-2xl lg:text-center lg:mb-10">
           {t("descriptionSkills")}
         </h3>
-        {items.map((item) => (
-          <div className={`bg-[${item.color}]`}>
-            <Tab config={{ items }} />
-          </div>
-        ))}
+        <Tab config={{ items }} />
       </section>
     </>
   );
